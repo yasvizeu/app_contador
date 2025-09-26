4f38625451107a43b25ee3e048950c26f28b0972
yasvizeu <yasminvizeusantana@yahoo.com>
2025-09-26 11:00:55 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,23 +9,81 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      home: HomePage()
+    );
   }
 }
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//     home: Container(color: const Color.fromARGB(255, 75, 0, 94),
+//     alignment: Alignment.center,
+//     child: Text('Olá, mundo'),)
+//   );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _peopleCount = 0;
+
+
+  void _incrementPeople(){
+    setState(() {
+      _peopleCount++;
+    });
+  }
+
+  void _decrementPeople(){
+    setState(() {
+      _peopleCount--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Container(color: const Color.fromARGB(255, 75, 0, 94),
-    alignment: Alignment.center,
-    child: Text('Olá, mundo'),)
-  );;
+    return Scaffold(
+      appBar: AppBar(title: Text('Olá, mundo')),
+        backgroundColor: Colors.black,
+      body: Column(
+        // mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Pode Entrar!',
+          style: TextStyle(
+            fontSize:30,
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            letterSpacing: 4,
+          )),
+          Text('Pessoas = $_peopleCount', 
+          style:TextStyle(
+            fontSize:30,
+            color: Colors.white,
+          )),
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(onPressed: _incrementPeople,
+              child: Text('entrar', style: TextStyle(color: Colors.white,),),),
+              TextButton(onPressed: _decrementPeople,
+              child: Text('sair', style:TextStyle(color: Colors.white,)))
+            ]
+          )
+          ,
+        ],),
+    );
   }
 }
-
-/*Crie um novo stateless com o nome HomePage
-Tranfira o conteúdo de MaterialAoo para HomePage
-Carregar HomePage em MyApp*/ 
