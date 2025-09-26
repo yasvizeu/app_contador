797f3512c74b5d544e5c51b7097e0c838bdc9276
yasvizeu <yasminvizeusantana@yahoo.com>
2025-09-26 09:10:32 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Container(color: const Color.fromARGB(255, 75, 0, 94),
-    alignment: Alignment.center,
-    child: Text('Olá, mundo'),)
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +9,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return HomePage();
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Container(color: const Color.fromARGB(255, 75, 0, 94),
+    alignment: Alignment.center,
+    child: Text('Olá, mundo'),)
+  );;
+  }
+}
+
+/*Crie um novo stateless com o nome HomePage
+Tranfira o conteúdo de MaterialAoo para HomePage
+Carregar HomePage em MyApp*/ 
